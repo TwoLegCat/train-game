078c933d622b07c6f3f68c59393078bde6ac601b
TwoLegCat <97408692+TwoLegCat@users.noreply.github.com>
2023-05-14 23:39:11 +0200

    Sync with other devices

--- src/Map.ts
+++ src/Map.ts
@@ -2,9 +2,31 @@ export class Map {
     width: number;
     height : number;
     content : object[][] = [[{}]];
-    //c:Array<Array<object>>
     constructor(x : number, y : number) {
         this.width = x;
         this.height = y;
+    }
+    create(districtCount: number, tpd: number, ratio: number) {
+        let totalTiles = districtCount * tpd;  
+        let nearestCleanTotalTiles = Math.round(Math.sqrt(tpd)) ** 2
+        let divisors : Array<number> = [];
+        for (let i = 1; i <= totalTiles; i++) {
+            if (totalTiles % i === 0) {
+                divisors.push(i);
+            }
+        }
+            
+          
+        let map : Array<Array<number>> = Array(5).fill(Array(5).fill(0));
+        
+        console.log(map)
+        let numTiles : Array<Array<number>> = [[1, 1, 1, 1, 1], [2, 2, 2, 2, 2], [3, 3, 3, 3, 3], [4, 4, 4, 4, 4], [5, 5, 5, 5, 5]];
+        
+        for (let i = 0; i < numTiles.length; i++) {
+            for (let j = 0; j < numTiles[i].length; j++) {
+                let area = Math.ceil(tpd ** (1/4))
+            }
+        }
+
     }
 }
--- src/createfield.ts
+++ src/createfield.ts
@@ -1,12 +1,5 @@
-const districts: string = "abcxyz";
-let tpd: number;
-let ratio: number;
-interface Dimensions {
-    x: number;
-    y: number;
-}
-const defineDimensions = (districtCount: number, tpd: number, ratio: number): number[] => {
-    let totalArea : number = districtCount * tpd * (ratio + 1);
+const defineDimensions = (districtCount: number, tpd: number, ratio: number): Array<number> => {
+    let totalArea : number = districtCount * tpd /** (ratio + 1); */
     let length : number = Math.round(4 * Math.sqrt(totalArea) / 3);
     let height : number = Math.round(3 * Math.sqrt(totalArea) / 4);
     let obstructions : number = length * height - districtCount * tpd;
