9d7ee754b8dd75dd24a3b8ce48c8eb14f74316af
TwoLegCat <97408692+TwoLegCat@users.noreply.github.com>
2023-05-30 17:09:48 +0200

    Improved map making

---
+++ src/Map.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Map.css';
+import Map from './createMap.ts' 
+
+function Map() {
+    let map = new Map();
+    return(
+        <canvas></canvas>
+    );
+}
--- src/createMap.ts
+++ src/createMap.ts
@@ -1,11 +1,10 @@
 import Tile from './Tile';
 import District from './District';
-export class Map {
+export default class Map {
     width: number;
     height : number;
     content : Array<Array<Tile>>;
     districts: Array<District> = [];
-    //districts : Array<Array<{name : string, tiles : number}>>;
     constructor(totalTiles: number, districtCount: number, distanceBetweenStartTiles: number, ratio: number) {
         //define width and height
         this.width = Math.ceil(Math.sqrt(totalTiles * 16/9));
@@ -17,9 +16,10 @@ export class Map {
         //define names for districts
         let districtNames: Array<string> = [];
         let startTiles: Array<Array<number>> = [];
-        let abc = "abcdefghijklmnopqrstuvwxyz";
+        let chars = "abcdeghkmnopqrsuvwxyz";
+        //too many districts,names,tiles?
         while (startTiles.length < districtCount) {
-            let generatedName : string = `${abc[(Math.random() * abc.length)]}${abc[(Math.random() * abc.length)]}`;
+            let generatedName : string = `${chars[(Math.random() * chars.length)]}${chars[(Math.random() * chars.length)]}`;
             if (!districtNames.includes(generatedName)) {
                 districtNames.push(generatedName)
                 let rx = ~~(Math.random() * this.width);
@@ -28,15 +28,15 @@ export class Map {
                     if (Math.sqrt((rx-startTiles[i][0])**2 + (ry-startTiles[i][1])**2) > distanceBetweenStartTiles) {
                         startTiles.push([rx, ry]);
                         this.content[rx][ry] = new Tile(rx, ry, generatedName);
+                    } else {
+                        districtNames.length -= 1;
                     }
                 }
-
             }
         }
         for (let i = 0; i < startTiles.length; i++) {
             this.districts[i] = new District(districtNames[i], startTiles[i], 0)
         }
-        
     }
 }
 console.log(new Map(4000, 10, 1, 1))
