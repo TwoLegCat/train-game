9fa571117cfa8e527833152ede6456345f956d47
TwoLegCat <97408692+TwoLegCat@users.noreply.github.com>
2023-06-02 21:26:17 +0200

    New Stuff

--- src/components/Map_Visuals.tsx
+++ src/components/Map_Visuals.tsx
@@ -4,11 +4,12 @@ import mapConstruct from '../createMap'
 const map: mapConstruct = new mapConstruct(10000, 26);
 
 function Map() {
-    let tileSize = 16;
+    const [tileSize, setTileSize] = useState(16);
     const size = {width: map.width * tileSize, height: map.height * tileSize}
+    const canvRef = useRef(null);
     return(
-        <canvas id="map" {...size} ></canvas>
+        <canvas id="map" {...size} ref={canvRef}></canvas>
     );
-}
+}  
 
 export default Map;
