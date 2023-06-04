85d6da2dcd21efbf51dc44911fb0224c4692403c
TwoLegCat <97408692+TwoLegCat@users.noreply.github.com>
2023-06-04 17:03:07 +0200

    Visualized Map

--- src/components/MapVisuals.tsx
+++ src/components/MapVisuals.tsx
@@ -1,7 +1,22 @@
 import React, {useEffect, useRef, useState} from 'react';
 import '../css/MapVisuals.css';
 import Map from '../createMap' 
-const map : Map = new Map(600, 3);
+const colors : Array<Array<string>> = [
+    [
+        "#121221", "#1d252d", "#1f2344", "#19334c", "#192633", 
+        "#b0c4d8", /*"#b3b3d5",*/ "#9c9cc9", /*"#8484bc",*/ "#6d6daf", 
+        "#5858a1", /*"#4b4b8a",*/ "#3f3f73", /*"#32325c",*/ "#b7c4d1", 
+        "#a1b2c3", /*"#8ca0b5",*/ "#768ea7", /*"#617c97",*/ "#536b82", 
+        "#45596c", /*"#374756",*/ "#aeb3da", /*"#959ccf",*/ "#7d85c3", 
+        "#646db8", /*"#4e58ab",*/ "#434b93", /*"#373f7a",*/ "#2c3262",
+        "#a6c4e1", /*"#8bb3d9",*/ "#71a1d0", /*"#5681c7",*/ "#3d7ebc",
+        "#356ca1", /*"#2c5a86",*/ "#23486b", /*"#98b2cc",*/ "#80a0c0", 
+        "#688eb5", /*"#527ca7",*/ "#466b81", /*"#3a5977",*/ "#2e4751", 
+        "#252545", /*"#19192e",*/ "#293541", /*"#1b232b",*/ "#212549", 
+        "#161931", /*"#1a3650",*/ "#112435", /*"#233547",*/ "#17232f"
+    ]
+];
+const map : Map = new Map(11000, 8, colors[0]);
 function MapVisuals() {
     const [tileSize, setTileSize] = useState(16);
     
@@ -11,7 +26,6 @@ function MapVisuals() {
     useEffect(() => {//execute after first render once 
         const canvas = canvRef.current; if(!canvas) return;
         const ctx = canvas.getContext("2d"); if(!ctx) return;
-        console.log(map.content)
         for (let x = 0; x < map.width; x++) {
             for (let y = 0; y < map.height; y++) {
                 ctx.fillRect(x * tileSize, y * tileSize, tileSize, tileSize);
--- src/createMap.ts
+++ src/createMap.ts
@@ -5,66 +5,58 @@ export default class Map {
     height : number;
     content : Array<Array<Tile>>;
     districts : Array<District> = [];
-    constructor(totalDesiredTiles: number, districtCount: number, ratio?: number) {
+    colors: Array<string>;
+    constructor(totalDesiredTiles: number, districtCount: number, colors : Array<string>, ratio?: number) {
         //define width and height
         this.width = Math.ceil(Math.sqrt(totalDesiredTiles * 16/9));
         this.height = Math.ceil(Math.sqrt(totalDesiredTiles * 9/16));
+        this.colors = colors
         let totalTiles = this.width * this.height;
         let distanceBetweenStartTiles = Math.sqrt(totalTiles / districtCount) //TODO: still unclear, maybe too less space
-        //initialize Map array
-        console.log(this.width, this.height);
         //define names for districts
         let districtNames: Array<string> = [];
         let startTilePositions: Array<Array<number>> = [];
         let chars = "abcdeghkmnopqrsuvwxyz";
-        const colors : Array<string> = ["#121221", "#8ee5c4", "#5076b7"];
         //too many districts,names,tiles?
         startTilePositions[0] = [~~(Math.random() * this.width), ~~(Math.random() * this.height)];
-        console.log(startTilePositions)
         districtNames[0] = `${chars[~~(Math.random() * chars.length)] + chars[~~(Math.random() * chars.length)]}`;
-        console.log(districtNames);
         let failed = false;
         while (startTilePositions.length < districtCount) {
             let generatedName : string = `${chars[~~(Math.random() * chars.length)] + chars[~~(Math.random() * chars.length)]}`;
-            console.log(generatedName)
             if (!(districtNames.includes(generatedName))) {
                 districtNames.push(generatedName);
-                console.log(districtNames.length)
                 let rndpos = [~~(Math.random() * this.width), ~~(Math.random() * this.height)];
                 
                 for (let i = 0; i < startTilePositions.length; i++) {
-                    console.log("i=" + i);
                     if (Math.sqrt((rndpos[0] - startTilePositions[i][0])**2 + (rndpos[1] - startTilePositions[i][1])**2) > distanceBetweenStartTiles) {
-                        console.log("Success")
+                        console.log("random position passed random startTile")
                       
                     } else {
                         failed = true;
                         districtNames.length -= 1;
+                        console.error("random position was too close to antother startTile. Existing Positions: " + districtNames.length)
                         break;
                     }
                 }
                 if(failed) {failed = false; continue}
-                console.log("new tile created wse")
                 startTilePositions.push(rndpos);
+                console.warn("New Tile was created. Existing Positions: " + startTilePositions.length)
             }
         }
-        console.log(startTilePositions.length)
         for (let i = 0; i < startTilePositions.length; i++) {
             this.districts[i] = new District(districtNames[i], new Tile(startTilePositions[i], districtNames[i], colors[i]), colors[i]);
         }
-        console.log(this.districts);
         this.content = [];
         for (let x = 0; x < this.width; x++) {
             this.content[x] = [];
             for (let y = 0; y < this.height; y++) {
-                let distances : Array<{districtName : string, districtColor: string, distance : number}> = [];
+                let bestDistricts : Array<{districtName : string, districtColor: string, distance : number}> = [];
                 for (let i = 0; i < this.districts.length; i++) {
-                    distances[i] = {districtName: this.districts[i].name, districtColor: this.districts[i].color, distance: Math.sqrt((x-this.districts[i].startTile.pos[0])**2 + (y-this.districts[i].startTile.pos[1])**2)}
+                    bestDistricts[i] = {districtName: this.districts[i].name, districtColor: this.districts[i].color, distance: Math.sqrt((x - this.districts[i].startTile.pos[0]) ** 2 + (y - this.districts[i].startTile.pos[1]) ** 2)}
                 }
-                distances.sort((a, b) => a.distance - b.distance);
-                this.content[x][y] = (new Tile([x, y], distances[0].districtName, distances[0].districtColor));
+                bestDistricts.sort((a, b) => a.distance - b.distance);
+                this.content[x][y] = (new Tile([x, y], bestDistricts[0].districtName, bestDistricts[0].districtColor));
             }
         }
-        console.log(this.content)
     }
 }
